6a79bea77b8c56bff5f1dd83a7143d69cebea037
Edyta2801 <edyta.szarowska@interia.pl>
2022-01-24 14:26:49 +0100

    fix eslint and add Chakra ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { Container, SimpleGrid, Box, Text } from "@chakra-ui/react";
+
+const income = 15000;
+
 function App() {
+  const renderMonth = new Date().toLocaleDateString("pl-PL", { month: "long" });
   return (
-    <div className="App">
-      <div>Heloo!!!!</div>
-    </div>
-  )
+    <Container maxW="xl" centerContent marginTop="20">
+      <SimpleGrid columns={2} spacing={10}>
+        <Box bg="tomato" h="50px" borderRadius={30} px={6} py={2}>
+          <Text align="center" fontSize="xl" color="white">
+            {income}
+          </Text>
+        </Box>
+        <Box bg="orange" h="50px" borderRadius={30} px={6} py={2}>
+          <Text align="center" fontSize="xl" color="white">
+            {renderMonth}
+          </Text>
+        </Box>
+      </SimpleGrid>
+    </Container>
+  );
 }
 
-export default App
+export default App;
