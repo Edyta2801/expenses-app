35ef569e1a8d0e8832904eae65c15d05ddbd6087
Edyta2801 <edyta.szarowska@interia.pl>
2022-01-25 21:31:42 +0100

    add form

--- src/App.js
+++ src/App.js
@@ -1,22 +1,157 @@
-import { Container, SimpleGrid, Box, Text } from "@chakra-ui/react";
-
+import {
+  Container,
+  SimpleGrid,
+  Box,
+  Text,
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+  Select,
+} from "@chakra-ui/react";
+import { Formik, Form } from "formik";
 const income = 15000;
+
+const EXPENSE_TYPE = {
+  ONE_TIME: "once",
+  FIXED: "fixed",
+};
+const CATEGORY_OPTIONS = [
+  { key: "jedzenie", text: "jedzenie", value: "jedzenie" },
+  { key: "inne", text: "inne", value: "inne" },
+  { key: "firma", text: "firma", value: "firma" },
+  { key: "mieszkanie", text: "mieszkanie", value: "mieszkanie" },
+];
+
+const EXPENSE_TYPE_OPTIONS = [
+  { key: EXPENSE_TYPE.FIXED, text: "powtarza się", value: EXPENSE_TYPE.FIXED },
+  {
+    key: EXPENSE_TYPE.ONE_TIME,
+    text: "jednorazowy",
+    value: EXPENSE_TYPE.ONE_TIME,
+  },
+];
 
 function App() {
   const renderMonth = new Date().toLocaleDateString("pl-PL", { month: "long" });
+
   return (
-    <Container maxW="xl" centerContent marginTop="20">
-      <SimpleGrid columns={2} spacing={10}>
-        <Box bg="tomato" h="50px" borderRadius={30} px={6} py={2}>
+    <Container maxW="xl" centerContent marginTop="10">
+      <SimpleGrid columns={1} spacing={5} minW="sm">
+        <Box bg="tomato" h="45px" borderRadius={10} px={6} py={2}>
           <Text align="center" fontSize="xl" color="white">
             {income}
           </Text>
         </Box>
-        <Box bg="orange" h="50px" borderRadius={30} px={6} py={2}>
+        <hr />
+        <Box bg="orange" h="45px" borderRadius={10} px={6} py={2}>
           <Text align="center" fontSize="xl" color="white">
             {renderMonth}
           </Text>
         </Box>
+
+        <Formik
+          initialValues={{
+            category: "jedzenie",
+            expense: "",
+            howMuch: "",
+            //  expenseType: EXPENSE_TYPE.ONE_TIME,
+            expenseType: " EXPENSE_TYPE.ONE_TIME",
+          }}
+          onSubmit={(values, { setSubmitting, resetForm }) => {
+            setSubmitting(true);
+            resetForm();
+          }}
+        >
+          {({
+            values,
+            handleChange,
+            handleSubmit,
+            isSubmitting,
+            setFieldValue,
+          }) => (
+            <Form onSubmit={handleSubmit}>
+              <FormControl isRequired my="2">
+                <FormLabel htmlFor="category">Kategoria</FormLabel>
+                <Select
+                  id="category"
+                  placeholder="Kategoria"
+                  option={CATEGORY_OPTIONS}
+                  value={values.category.text}
+                  onChange={(e, { value }) => setFieldValue("category", value)}
+                  required
+                  search
+                  error={Boolean(false)}
+                >
+                  {CATEGORY_OPTIONS.map((category) => (
+                    <option key={category.key} value={category.value}>
+                      {category.text}
+                    </option>
+                  ))}
+                </Select>
+              </FormControl>
+
+              <FormControl isRequired my="2">
+                <FormLabel htmlFor="expense">Wydatek</FormLabel>
+                <Input
+                  id="expense"
+                  label="wydatek"
+                  placeholder="wydatek"
+                  value={values.expense}
+                  onChange={handleChange}
+                  required
+                  error={Boolean(false)}
+                ></Input>
+              </FormControl>
+
+              <FormControl isRequired my="2">
+                <FormLabel htmlFor="howMuch">Kwota</FormLabel>
+                <Input
+                  id="howMuch"
+                  label="kwota"
+                  placeholder="kwota"
+                  type="number"
+                  step={0.01}
+                  value={values.howMuch}
+                  onChange={handleChange}
+                  required
+                  error={Boolean(false)}
+                ></Input>
+              </FormControl>
+
+              <FormControl isRequired my="2">
+                <FormLabel htmlFor="expenseType">Typ wydatku</FormLabel>
+                <Select
+                  id="expenseType"
+                  placeholder="Typ wydatku"
+                  option={EXPENSE_TYPE_OPTIONS}
+                  value={values.expenseType.text}
+                  onChange={(e, { value }) =>
+                    setFieldValue("expenseType", value)
+                  }
+                  required
+                  search
+                  error={Boolean(false)}
+                >
+                  {EXPENSE_TYPE_OPTIONS.map((expense) => (
+                    <option key={expense.key} value={expense.value}>
+                      {expense.text}
+                    </option>
+                  ))}
+                </Select>
+              </FormControl>
+              <Button
+                w="100%"
+                type="submit"
+                disabled={isSubmitting}
+                mt={4}
+                colorScheme="teal"
+              >
+                OK
+              </Button>
+            </Form>
+          )}
+        </Formik>
       </SimpleGrid>
     </Container>
   );
